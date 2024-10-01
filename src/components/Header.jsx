c74334a022ac9c1e1f55a9dfc71d47038a230ff6
mishrabhi <asmyselfabhishek00@gmail.com>
2024-10-01 13:57:03 +0530

    created banner section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         </div>
         <a
           className="flex item-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm  m-auto md:m-0 hover:scale-105 transition-all duration-300"
-          href=""
+          href="#speciality"
         >
           Book Appointment
           <img className="w-3" src={assets.arrow_icon} alt="" />
