0eca698d6ca061099816ebccfe3c615b63140a06
mishrabhi <asmyselfabhishek00@gmail.com>
2025-01-09 17:14:56 +0530

    created user register and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const { backendUrl, token, setToken } = useContext(AppContext);
+  const navigate = useNavigate();
   const [state, setState] = useState("Sign Up"); //checking state whether user need to login or create account
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -9,10 +15,44 @@ const Login = () => {
   //Handle Submit
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+    try {
+      if (state == "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          setToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          setToken(data.token);
+        } else {
+          toast.error(error);
+        }
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
+  useEffect(() => {
+    if (token) {
+      navigate("/"); //HomePage
+    }
+  }, [token]);
+
   return (
-    <form className="min-h-[89vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[89vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
         <p className="text-2xl font-semibold">
           {state === "Sign Up" ? "Create Account" : "Login"}
@@ -27,7 +67,7 @@ const Login = () => {
             <input
               className="border border-zinc-300 rounded w-full p-2 mt-1"
               type="text"
-              onClick={(e) => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               value={name}
               required
             />
@@ -39,7 +79,7 @@ const Login = () => {
           <input
             className="border border-zinc-300 rounded w-full p-2 mt-1"
             type="email"
-            onClick={(e) => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             value={email}
             required
           />
@@ -49,12 +89,15 @@ const Login = () => {
           <input
             className="border border-zinc-300 rounded w-full p-2 mt-1"
             type="password"
-            onClick={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             value={password}
             required
           />
         </div>
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+        <button
+          type="submit"
+          className="bg-primary text-white w-full py-2 rounded-md text-base"
+        >
           {state === "Sign Up" ? "Create Account" : "Login"}
         </button>
         {state === "Sign Up" ? (
